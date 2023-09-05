06ac3ce712654e719559c7c91fc4d7f6ca0094a8
Manuel Del Pozo <143064005+dev-madepozo@users.noreply.github.com>
2023-09-05 12:56:50 -0500

    The solution for the challenge #14

--- 14-challenge.js
+++ 14-challenge.js
@@ -1,1 +1,13 @@
+/**
+ * Challenge # 14
+ * URL: https://adventjs.dev/es/challenges/2022/14
+ */
 
+
+function getOptimalPath(path) {
+  return path.reduceRight((acc, cur) => {
+    return cur.map((v, i) => {
+      return v + Math.min(acc[i], acc[i + 1])
+    })
+  }).at(0)
+}
